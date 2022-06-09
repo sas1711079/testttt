50d29ffaf44ba36d8088b3f809a306ff049329dc
sas1711079 <104480818+sas1711079@users.noreply.github.com>
2022-06-09 11:42:07 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,31 @@
-music.setVolume(255)
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )))
-    basic.showNumber(sonar.ping(
+    if (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ))
+    ) > 1) {
+        basic.showLeds(`
+            . # # . .
+            # . . # .
+            # # # # .
+            # . . # .
+            # . . # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # # .
+            # . . # .
+            # # # . .
+            `)
+    }
 })
 basic.forever(function () {
-    music.playTone(880, music.beat(BeatFraction.Half))
-    basic.pause(200)
-    music.playTone(988, music.beat(BeatFraction.Half))
+	
 })
